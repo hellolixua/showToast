72e573168cf0d1adbc02d53c8c96218eb550431c
lx <1063040087@qq.com>
2020-05-16 12:52:12 +0800

    init showToast npm

---
+++ examples/App.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {},
+  mounted() {
+    this.$showToast.show('你好2020!')
+    // this.$showToast.show()
+    // setTimeout(() => {
+    //   this.$showToast.hide()
+    // }, 3000);
+  }
+}
+</script>
